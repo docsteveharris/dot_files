f50809fd4c36adcb0157859689d0d11f2a824bf3
Steve Harris <git@steveharris.me>
2019-08-10 12:51:55 +0100

    converted from function to script

--- prezto.sh
+++ prezto.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 
-prezto.sh(){
-    clear
-    sudo apt-get update && sudo apt-get install -y zsh
-    # Get prezto
-	git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
-    
-    # Backup zsh config if it exists
-    if [ -f ~/.zshrc ];
-       then
-           mv ~/.zshrc ~/.zshrc.backup
-    fi
 
-    # Create links to zsh config files
-    setopt EXTENDED_GLOB
-    for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
-      ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
-    done
-    
-    chsh -s /bin/zsh
-}
+clear
+sudo apt-get update && sudo apt-get install -y zsh
+# Get prezto
+git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+
+# Backup zsh config if it exists
+if [ -f ~/.zshrc ];
+   then
+       mv ~/.zshrc ~/.zshrc.backup
+fi
+
+# Create links to zsh config files
+setopt EXTENDED_GLOB
+for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+done
+
+chsh -s /bin/zsh
